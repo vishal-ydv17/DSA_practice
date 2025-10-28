5573c43770ee138c82f469eedea4fd644c7de311
Vishal Yadav <vishalyadavy0022@gmail.com>
2025-10-28 11:28:43 +0530

    largest elementin arr with edge cases

--- 01_lets_warmup/secondLargest.js
+++ 01_lets_warmup/secondLargest.js
@@ -2,11 +2,16 @@ function secondLargest(arr) {
     let firstHigh= -Infinity; // to store the largest number
     let secondHigh= -Infinity; // to store the second largest number
 
+    if(arr.length < 2) return null; // edge case: if array has less than 2 elements
+
     for (let i = 0; i < arr.length; i++) {
         let current= arr[i];
         if(current > firstHigh){
             secondHigh= firstHigh; // update second largest
             firstHigh= current; // update largest
+        }
+        else if(current > secondHigh && current < firstHigh){
+            secondHigh= current; // update second largest only
         }
     }
     
