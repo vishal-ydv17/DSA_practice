4445635951dde82b0678f923cad52d72e8df1e9c
Vishal Yadav <vishalyadavy0022@gmail.com>
2026-08-13 16:07:31 +0530

    same tree

---
+++ 11_Binary Tree/100. Same Tree/code.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    
+    if(!p && !q) return true;//if at leaf both null then give true
+    if(!p || !q) return false;//if either of node is thre then false
+
+    if(p.val!=q.val) return false;//both node should be equal given in Q
+
+    //now recursively call for left and right subtree
+    return  isSameTree(p.left, q.left) && 
+            isSameTree(p.right, q.right);
+};
